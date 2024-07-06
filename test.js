086769cb5e145c48c3e2697ca617d5174cba57bc
voyager-05 <32582624+voyager-05@users.noreply.github.com>
2024-07-06 22:55:27 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,51 +1,85 @@
 const sheetID='1JSjqrH2QGnOwns5hOsWQaYgppDRjWzmP05ZZCUqVVnY';
-const base=`https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
-const sheetName='Attendance';
-let point = 150;
-//let qu = `Select A,C,D WHERE D > ${point}`;
-// qu = 'Select * WHERE B ="Svekis"';
-// qu = 'Select * WHERE A contains "Jo"';
-//let qu = 'Select * WHERE A contains "Jo"';
-// qu = 'Select * WHERE E > date "2021-12-31"';
-let qu = 'Select * WHERE C = "active" And B = "Svekis"';
-const query = encodeURIComponent(qu);
-const data = [];
-const url= `${base}&sheet=${sheetName}&tq=${query}`;
-document.addEventListener('DOMContentLoaded', init);
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
+const sheetName = 'Sheet2';
 
+document.addEventListener('DOMContentLoaded', init);
  
 const output = document.querySelector('.output');
-
-function init(){
+ 
+function init() {
     console.log('ready');
-    point = 300;
+    clearBox();
+
+    if (document.getElementById("sID").value.length == 0)
+    {
+        console.log("XXXXXXXXX");
+        return;
+    }
+
+    if (document.getElementById("sID").value.length != 6)
+    {
+        console.log("WWWWWWW");
+        output.innerHTML = "<br /><br />ID SHOULD BE 6 DIGITS!!!";
+        return;
+    }
+    
+    let sID = parseInt(document.getElementById("sID").value, 10);
+
+    // let sID_s = sID.toString();
+
+    let qu = `Select * WHERE A = ${sID}`;
+    //let qu = `Select A,C,D WHERE D >= ${point}`;
+    // qu = 'Select * WHERE B ="Svekis"';
+    // qu = 'Select * WHERE A contains "Jo"';
+    // qu = 'Select * WHERE A contains "Jo"';
+    // qu = 'Select * WHERE E > date "2021-12-31"';
+    // qu = 'Select * WHERE C = "active" And B = "Svekis"';
+    const query = encodeURIComponent(qu);
+    const url = `${base}&sheet=${sheetName}&tq=${query}`;
+    const data = [];
     fetch(url)
-    .then(res => res.text())
-    .then(rep => {
-        //console.log(rep);
-        const jsData = JSON.parse(rep.substring(47).slice(0, -2));
-        console.log(jsData);
-        const colz = [];
-        jsData.table.cols.forEach((heading) => {
-            if(heading.label) {
-                colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
+        .then(res => res.text())
+        .then(rep => {
+            console.log(rep)
+            //console.log(rep);
+            const jsData = JSON.parse(rep.substr(47).slice(0, -2));
+            if (jsData.table.rows != 0)
+            {
+                console.log(jsData);
+                const colz = [];
+                jsData.table.cols.forEach((heading) => {
+                    if (heading.label) {
+                        colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
+                    }
+                })
+                jsData.table.rows.forEach((main) => {
+                    //console.log(main);
+                    const row = {};
+                    colz.forEach((ele, ind) => {
+                        //console.log(ele);
+                        row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
+                    })
+                    data.push(row);
+                })
+                maker(data);
             }
-        })
-        jsData.table.rows.forEach((main)=>{
-            console.log(main);
-            const row = {};
-            colz.forEach((ele, ind) => {
-                console.log(ele);
-                row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
-            })
-            data.push(row);
-        })
-        maker(data);
-    })
+            else
+            {
+                output.innerHTML = "<br /><br />NO SUCH ID FOUND!!!";
+            }
+        })    
+}
+ 
+function clearBox()
+{
+    output.innerHTML = "";
 }
 
 function maker(json) {
     const div = document.createElement('div');
+
+    output.innerHTML = "";
+    output.innerHTML = "<br /><br />"
     div.style.display = 'grid';
  
     output.append(div);
@@ -75,3 +109,4 @@ function maker(json) {
     })
  
 }
+ 
