29baca9f00444166d7dc26f1f9230f663f8488ab
voyager-05 <32582624+voyager-05@users.noreply.github.com>
2024-07-11 01:16:54 +0100

    Update test.js

--- intro-to-ai/test.js
+++ intro-to-ai/test.js
@@ -89,10 +89,10 @@ function init() {
 
 function isIOSSafari()
 {
-  var ua = window.navigator.userAgent;
-  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-  var webkit = !!ua.match(/WebKit/i);
-  return iOS && webkit && !ua.match(/CriOS/i);
+  //var ua = window.navigator.userAgent;
+  //var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  //var webkit = !!ua.match(/WebKit/i);
+  return navigator.platform === 'iPad' || navigator.platform === 'iPhone' || navigator.platform === 'iPod'; //iOS && webkit && !ua.match(/CriOS/i);
 }
 
 function isMobile() {
