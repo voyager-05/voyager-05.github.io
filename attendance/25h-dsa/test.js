9f0e3a25971bbf43927f05b0f697e891372ca80d
voyager-05 <32582624+voyager-05@users.noreply.github.com>
2025-06-05 10:51:42 +0200

    rename 25V to 25H

--- attendance/25h-dsa/test.js
+++ attendance/25h-dsa/test.js
@@ -1,6 +1,6 @@
 const sheetID='1JSjqrH2QGnOwns5hOsWQaYgppDRjWzmP05ZZCUqVVnY';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
-const sheetName = '25V - DSA Attendance_Register';
+const sheetName = '25H - DSA Attendance_Register';
 
 document.addEventListener('DOMContentLoaded', init);
  
