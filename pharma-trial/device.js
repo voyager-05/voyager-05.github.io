70bdb352e15b79fcba439eb789bbfa99b05c9f28
Kevin Tan <voyager05@hotmail.co.uk>
2025-10-24 18:45:41 +0200

    Another try out with spinning

--- pharma-trial/device.js
+++ pharma-trial/device.js
@@ -1,6 +1,6 @@
 export function getDeviceInfo() {
     return {
-        touch: matchMedia('(pointer:coarse)').matches,
+        touch: matchMedia('(pointer: coarse)').matches || 'ontouchstart' in window,
         width: window.innerWidth,
         height: window.innerHeight,
         dpr: window.devicePixelRatio || 1
