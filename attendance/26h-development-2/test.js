f1a280a18e33b9c754149e4e9fd914031b6e0cb8
Kevin Tan <7532768+kevin4@users.noreply.github.com>
2026-07-25 19:47:30 +0000

    Update_v2

--- attendance/26h-development-2/test.js
+++ attendance/26h-development-2/test.js
@@ -1,6 +1,6 @@
 const sheetID='1JSjqrH2QGnOwns5hOsWQaYgppDRjWzmP05ZZCUqVVnY';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
-const sheetName = 'Copy of 25H - ML_AI Attendance_Register';
+const sheetName = '26H - ML_AI Attendance_Register';
 
 document.addEventListener('DOMContentLoaded', init);
  
@@ -12,7 +12,7 @@ function init() {
 
     if (document.getElementById("sID").value.length == 0)
     {
-        console.log("BBBBBBBBB");
+        console.log("CCCCCC");
         return;
     }
 
