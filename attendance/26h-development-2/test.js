135fa312e22341eea4b6ef99549bd80a0cc500bf
Kevin Tan <7532768+kevin4@users.noreply.github.com>
2026-07-25 19:03:33 +0000

    Update_v2

--- attendance/26h-development-2/test.js
+++ attendance/26h-development-2/test.js
@@ -1,6 +1,6 @@
-const sheetID='1JSjqrH2QGnOwns5hOsWQaYgppDRjWzmP05ZZCUqVVnY';
+const sheetID='1IA1NSvOId2YyyJzrM3gokJwggBEqb2dnUX66JxUFwL0';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
-const sheetName = '26H - Development 2 Attendance_Register';
+const sheetName = '26H - Development-2-Attendance_Sheet-';
 
 document.addEventListener('DOMContentLoaded', init);
  
@@ -12,7 +12,7 @@ function init() {
 
     if (document.getElementById("sID").value.length == 0)
     {
-        console.log("SSSSSSSS");
+        console.log("TTTTTTT");
         return;
     }
 
